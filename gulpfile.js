c2de4b0964db547addccbd8a9972da2e1a65606c
Anil Maharjan <simpleanilcha@gmail.com>
2015-10-27 15:30:49 +0545

    Added new plugins like autoprefixer, pixrem, Opacity, rgba.. Fallbacks to css.

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,28 @@ var gulp = require ('gulp'),
 	postcss = require('gulp-postcss'),
 	autoprefixer = require('autoprefixer'),
 	cssnext = require('cssnext'),
-	precss = require('precss');
+	precss = require('precss'),
+	color_rgba_fallback = require('postcss-color-rgba-fallback'),
+	opacity = require('postcss-opacity'),
+	pseudoelements = require('postcss-pseudoelements'),
+	vmin = require('postcss-vmin'),
+	pixrem = require('pixrem'),
+	will_change = require('postcss-will-change');
 
 
 gulp.task('css', function(){
 	var processors = [
-	autoprefixer({browsers: ['last 1 version']}),
+	will_change,
+	autoprefixer,
+//	autoprefixer({browsers: ['last 1 version']}),
+//	autoprefixer({browsers:'safari >= 9, ie >= 11'}),
 	cssnext,
-	precss
+	precss,
+	color_rgba_fallback,
+	opacity,
+	pseudoelements,
+	vmin,
+	pixrem
 	];
 	return gulp.src('./src/*.css')
 	.pipe(postcss(processors))
